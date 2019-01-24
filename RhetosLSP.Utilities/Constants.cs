7969ef0515b4d014fe589597fddf9ce679b1ceed
HuyNguyen <nguyenquanghuy031295@gmail.com>
2019-01-24 16:09:16 +0700

    fix hover + complete keyword when typing space

---
+++ RhetosLSP.Utilities/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RhetosLSP.Utilities
+{
+    public static class Constants
+    {
+        public static readonly char[] CommitCharacters = new char[] { ' ', '{' };
+        public static readonly char[] StopCharacters = new char[] { '\n', ' ', '\r', ';' };
+    }
+}
---
+++ RhetosLSP.Utilities/WordOnHover.cs
@@ -0,0 +1,18 @@
+﻿using RhetosLSP.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RhetosLSP.Utilities
+{
+    public class WordOnHover
+    {
+        public string Word { get; set; }
+
+        public Position Start { get; set; }
+
+        public Position End { get; set; }
+    }
+}
